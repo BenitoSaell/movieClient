bee4375dca11506239fa4b69a8aa1312ba0059af
Beni <benito-1101@hotmail.com>
2019-03-24 17:48:42 -0600

    Adds url to constants file

---
+++ src/main/java/com/benitosaell/client/constant/Urls.java
@@ -0,0 +1,10 @@
+package com.benitosaell.client.constant;
+
+public class Urls {
+	private static String URL_BASE_API = "http://localhost:3000";
+	public static String URL_LOGIN = URL_BASE_API + "/login";
+	public static String URL_PUBLIC = URL_BASE_API + "/api/publico";
+	public static String URL_MOVIES = URL_BASE_API + "/api/peliculas";
+	public static String URL_USERS = URL_BASE_API + "/api/usuarios";
+
+}
